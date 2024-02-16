86b2227d549eabe9d3db8e0a5730a5c348d5243a
RishabhGoyal888 <159071663+RishabhGoyal888@users.noreply.github.com>
2024-02-16 19:35:22 +0530

    Time: 3 ms (61.15%) | Memory: 12.4 MB (41.61%) - LeetSync

--- 2pointers/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 2pointers/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -6,13 +6,15 @@ public:
         int end=n-1;
         while(start<=end){
             int mid = start + (end-start)/2;
-            int low = arr[(mid + n- 1)%n];
-            int high = arr[(mid + 1)%n];
-            if(arr[mid] <= low && arr[mid] <= high){
+            int left = arr[(mid + n- 1)%n];  //for taking care of boundary cases, added mod
+            int right = arr[(mid + 1)%n];
+            
+            if(arr[mid] <= left && arr[mid] <= right){  //this is the minimum element
                 return arr[mid];
             }
-
-            else if(arr[mid] <= arr[end]){
+            //answer will lie in the unsorted half as minimum element is the pivot for rotating
+            //so discarding the sorted half
+            else if(arr[mid] <= arr[end]){  //first checking second half, because if both halves will be sorted, means array already sorted, then answer will be in first half.
                 end = mid - 1;
             }
             else{
