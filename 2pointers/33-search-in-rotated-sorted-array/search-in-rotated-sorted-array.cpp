cc23e80d91b8145822549fea597ec364bc8186c6
RishabhGoyal888 <159071663+RishabhGoyal888@users.noreply.github.com>
2024-02-16 20:28:11 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (44.96%) - LeetSync

--- 2pointers/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 2pointers/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -4,20 +4,18 @@ public:
         int n = arr.size();
         int start = 0;
         int end = n-1;
-
         while(start <= end){
             int mid = start + (end-start)/2;
-
             if(arr[mid] == target)
                 return mid;
-
-            else if(arr[mid] <= arr[end]){
+        //checking which side is sorted, jaunsi side sorted ho usme hi compare kr skte hai ki ussme hai ya nhi.
+            else if(arr[mid] <= arr[end]){  //agr right sorted hai to usme compare krenge
                 if(target >= arr[mid] && target <= arr[end]){
                     start = mid+1;
                 }
                 else end = mid-1;
             }
-            else if(arr[start] <= arr[mid]){
+            else if(arr[start] <= arr[mid]){ //agr left sorted hai to usme compare krenge
                 if(target >= arr[start] && target <= arr[mid]){
                     end = mid-1;
                 }
