f2009bfba5be9667e17d0d448a983f5f028f5edd
RishabhGoyal888 <159071663+RishabhGoyal888@users.noreply.github.com>
2024-02-20 13:53:27 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (21.57%) - LeetSync

--- 2pointers/21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 2pointers/21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,3 +1,48 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+//         ListNode* dummy = new ListNode(-1);
+//         ListNode* curr = dummy;
+//         while(l1 && l2){
+//             if(l1->val <= l2->val){
+//                 ListNode* temp = new ListNode(l1->val);
+//                 curr->next = temp;
+//                 curr = temp;
+//                 l1=l1->next;
+//             }
+//             else{
+//                 ListNode* temp = new ListNode(l2->val);
+//                 curr->next = temp;
+//                 curr = temp;
+//                 l2=l2->next;
+//             }
+//         }
+//         while(l1){
+//             ListNode* temp = new ListNode(l1->val);
+//             curr->next = temp;
+//             curr = temp;
+//             l1=l1->next;
+//         }
+//         while(l2){
+//             ListNode* temp = new ListNode(l2->val);
+//             curr->next = temp;
+//             curr = temp;
+//             l2=l2->next;
+//         }
+//         return dummy->next;
+//     }
+// };
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -15,29 +60,20 @@ public:
         ListNode* curr = dummy;
         while(l1 && l2){
             if(l1->val <= l2->val){
-                ListNode* temp = new ListNode(l1->val);
-                curr->next = temp;
-                curr = temp;
+                curr->next = l1;
                 l1=l1->next;
             }
             else{
-                ListNode* temp = new ListNode(l2->val);
-                curr->next = temp;
-                curr = temp;
+                curr->next = l2;
                 l2=l2->next;
             }
+            curr=curr->next;
         }
-        while(l1){
-            ListNode* temp = new ListNode(l1->val);
-            curr->next = temp;
-            curr = temp;
-            l1=l1->next;
+        if(l1){
+            curr->next = l1;
         }
-        while(l2){
-            ListNode* temp = new ListNode(l2->val);
-            curr->next = temp;
-            curr = temp;
-            l2=l2->next;
+        if(l2){
+            curr->next = l2;
         }
         return dummy->next;
     }
