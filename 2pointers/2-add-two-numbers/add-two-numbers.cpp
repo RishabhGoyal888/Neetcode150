73497c6ef0c8880605746f9296364154d968d719
RishabhGoyal888 <159071663+RishabhGoyal888@users.noreply.github.com>
2024-02-21 19:13:28 +0530

    Time: 22 ms (59.71%) | Memory: 76.1 MB (27.58%) - LeetSync

--- 2pointers/2-add-two-numbers/add-two-numbers.cpp
+++ 2pointers/2-add-two-numbers/add-two-numbers.cpp
@@ -1,3 +1,58 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+//         ListNode* t1=l1;
+//         ListNode* t2=l2;
+//         ListNode* dummy = new ListNode(-1);
+//         ListNode* curr = dummy;
+//         int sum=0;
+//         int carry=0;
+//         while(t1 && t2){
+//             sum=t1->val + t2->val + carry;
+//             ListNode* temp=new ListNode(sum%10);
+//             curr->next = temp;
+//             curr=temp;
+//             carry=sum/10;
+//             sum=0;
+//             t1=t1->next;
+//             t2=t2->next;
+//         }
+//         while(t1){
+//             sum=t1->val + carry;
+//             ListNode* temp=new ListNode(sum%10);
+//             curr->next = temp;
+//             curr=temp;
+//             carry=sum/10;
+//             sum=0;
+//             t1=t1->next;
+//         }
+//         while(t2){
+//             sum=t2->val + carry;
+//             ListNode* temp=new ListNode(sum%10);
+//             curr->next = temp;
+//             curr=temp;
+//             carry=sum/10;
+//             sum=0;
+//             t2=t2->next;
+//         }
+//         if(carry){
+//             ListNode* temp=new ListNode(carry);
+//             curr->next = temp;
+//         }
+
+//         return dummy->next;
+//     }
+// };
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -17,34 +72,19 @@ public:
         ListNode* curr = dummy;
         int sum=0;
         int carry=0;
-        while(t1 && t2){
-            sum=t1->val + t2->val + carry;
+        while(t1 || t2){
+            sum=carry;
+            if(t1) sum = sum + t1->val;
+            if(t2) sum = sum + t2->val;
             ListNode* temp=new ListNode(sum%10);
             curr->next = temp;
             curr=temp;
             carry=sum/10;
             sum=0;
-            t1=t1->next;
-            t2=t2->next;
+            if(t1) t1=t1->next;
+            if(t2) t2=t2->next;
         }
-        while(t1){
-            sum=t1->val + carry;
-            ListNode* temp=new ListNode(sum%10);
-            curr->next = temp;
-            curr=temp;
-            carry=sum/10;
-            sum=0;
-            t1=t1->next;
-        }
-        while(t2){
-            sum=t2->val + carry;
-            ListNode* temp=new ListNode(sum%10);
-            curr->next = temp;
-            curr=temp;
-            carry=sum/10;
-            sum=0;
-            t2=t2->next;
-        }
+        
         if(carry){
             ListNode* temp=new ListNode(carry);
             curr->next = temp;
