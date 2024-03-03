15963a0a7134244b01bdebcb48abe730e1a9c078
RishabhGoyal888 <159071663+RishabhGoyal888@users.noreply.github.com>
2024-03-03 22:10:35 +0530

    Time: 5 ms (26.20%) | Memory: 11.4 MB (11.44%) - LeetSync

--- 2pointers/226-invert-binary-tree/invert-binary-tree.cpp
+++ 2pointers/226-invert-binary-tree/invert-binary-tree.cpp
@@ -17,6 +17,7 @@ public:
         TreeNode* temp = root->left;
         root->left = root->right;
         root->right = temp;
+        
         invertTree(root->left);
         invertTree(root->right);
         return root;
