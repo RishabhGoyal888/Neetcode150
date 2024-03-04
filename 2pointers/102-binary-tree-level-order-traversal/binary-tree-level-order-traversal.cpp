479e44c07cd18ad5e1de57fc383f7a8010d4a2cb
RishabhGoyal888 <159071663+RishabhGoyal888@users.noreply.github.com>
2024-03-04 13:57:06 +0530

    Time: 6 ms (37.12%) | Memory: 15.1 MB (13.27%) - LeetSync

--- 2pointers/102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 2pointers/102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -22,7 +22,7 @@ public:
             for(int i=0;i<size;i++){
                 TreeNode* node = q.front();
                 q.pop();
-                if(node) list.push_back(node->val);
+                list.push_back(node->val);
                 if(node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
             }
