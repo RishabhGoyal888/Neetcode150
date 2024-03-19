fc74ea936cab6334bc6542fdcf0599f5fa255df5
RishabhGoyal888 <159071663+RishabhGoyal888@users.noreply.github.com>
2024-03-20 03:39:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (71.55%) - LeetSync

---
+++ 2pointers/46-permutations/permutations.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void swapf(int &a, int &b){
+        int temp = a;
+        a = b;
+        b = temp;
+        return;
+    }
+
+    void permutesubfunc(vector<int>& nums, vector<vector<int>>& ans, vector<int>& curr, int start){
+        if(start == nums.size()-1){
+            ans.push_back(nums);
+            return;
+        }
+
+        // permutesubfunc(nums, ans, curr, start + 1);
+
+        for(int i = start; i < nums.size(); i++){
+            swap(nums[start], nums[i]);
+            permutesubfunc(nums, ans, curr, start+1);
+            swap(nums[start], nums[i]);
+        }
+        return;
+
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+
+        permutesubfunc(nums, ans, curr, 0);
+        return ans;
+    }
+};
