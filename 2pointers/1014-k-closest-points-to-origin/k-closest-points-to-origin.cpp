f2103fe0381ea84df8c78f0d51372db0c1aca5e7
RishabhGoyal888 <159071663+RishabhGoyal888@users.noreply.github.com>
2024-04-14 01:49:03 +0530

    Time: 206 ms (25.54%) | Memory: 80.5 MB (32.36%) - LeetSync

--- 2pointers/1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 2pointers/1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,25 +1,3 @@
-// class Solution {
-// public:
-//     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-//         priority_queue<pair<int, pair<int, int>> > maxhp;
-//         for(int i=0;i<points.size();i++){
-//             int dis = points[i][0]*points[i][0] + points[i][1]*points[i][1];
-//             pair<int, pair<int,int> > p = { dis, {points[i][0],points[i][1]} };
-//             maxhp.push(p);
-//             if(maxhp.size() > k) maxhp.pop();
-//         }
-//         vector<vector<int>> res;
-//         while(maxhp.size() > 0 ){
-//             vector<int> curr;
-//             pair<int, int> currPoint = maxhp.top().second;
-//             maxhp.pop();
-//             curr.push_back(currPoint.first);
-//             curr.push_back(currPoint.second);
-//             res.push_back(curr);
-//         }
-//         return res;
-//     }
-// };
 
 class Solution {
 public:
