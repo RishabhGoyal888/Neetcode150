882186348596a8d66057467f04e04e7a4fa72af6
RishabhGoyal888 <159071663+RishabhGoyal888@users.noreply.github.com>
2024-04-14 01:32:50 +0530

    Time: 4 ms (37.37%) | Memory: 9.3 MB (16.96%) - LeetSync

---
+++ 2pointers/1127-last-stone-weight/last-stone-weight.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxhp;
+        for(int i=0;i<stones.size();i++){
+            maxhp.push(stones[i]);
+        }
+
+        while(maxhp.size() >= 2){
+            int y = maxhp.top();
+            maxhp.pop();
+            int x = maxhp.top();
+            maxhp.pop();
+            maxhp.push(y-x);
+            // if(y == x){
+            //     continue;
+            // } else {
+            //     maxhp.push(y-x);
+            // }
+        }
+        return maxhp.top();
+    }
+};
