29078be9f390c4262e13765f93b3e7d45f642f22
RishabhGoyal888 <159071663+RishabhGoyal888@users.noreply.github.com>
2024-06-23 16:22:48 +0530

    Time: 87 ms (86.16%) | Memory: 70 MB (65.07%) - LeetSync

--- 2pointers/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 2pointers/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -5,21 +5,30 @@ using namespace std;
 
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        int n = nums.size(); // Get the size of the input array
-        unordered_map<int, int> mpp; // Initialize unordered_map to store prefix sums
-        int sum = 0; // Initialize sum to count odd numbers
-        int ans = 0; // Initialize answer to count subarrays with exactly 'k' odd numbers
-        mpp[0] = 1; // Initialize map with 0 sum to handle cases where subarray starts from index 0
 
-        for (int i = 0; i < n; ++i) { // Iterate through the array
-            if (nums[i] % 2 == 1) { // Check if the current number is odd
-                sum += 1; // Increment the sum if the number is odd
+    int helpfx(vector<int>& nums, int k){
+        if(k<0) return 0;
+        int count = 0;
+        int ans = 0;
+        int i =0;
+        int j =0;
+        while(j<nums.size()){
+            if(nums[j] % 2 == 1){
+                count++;
             }
-            int diff = sum - k; // Calculate the difference needed to form a subarray with 'k' odd numbers
-            ans += mpp[diff]; // Add the count of subarrays found with the required difference
-            mpp[sum]++; // Update the map with the current sum
+            while(count>k){
+                if(nums[i] % 2 ==1){
+                    count--;
+                }
+                    i++;
+            }
+            ans += j-i+1;
+            j++;
         }
-        return ans; // Return the total count of subarrays with exactly 'k' odd numbers
+        return ans;
     }
-};
+
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return helpfx(nums,k) - helpfx(nums, k-1);
+    }
+}; 
