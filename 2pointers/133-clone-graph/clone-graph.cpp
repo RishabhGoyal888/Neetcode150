b664e881a9ee1368ad9676e4e0bc57aa0624d081
RishabhGoyal888 <159071663+RishabhGoyal888@users.noreply.github.com>
2025-01-18 15:53:28 +0530

    Time: 3 ms (68.96%) | Memory: 12.2 MB (30.01%) - LeetSync

--- 2pointers/133-clone-graph/clone-graph.cpp
+++ 2pointers/133-clone-graph/clone-graph.cpp
@@ -22,25 +22,22 @@ public:
 class Solution {
 public:
 
-    Node* dfs(Node* node, map<Node*, Node*> &mp){
-        if(node == NULL)
-            return NULL;
+    Node* dfs(map<Node*, Node*> &mp, Node* node){
+        if(node == NULL) return NULL;
 
-        if(mp.find(node) != mp.end()){
-            return mp[node];
+        if(mp.find(node) != mp.end()) return mp[node];
+
+        Node* copy = new Node(node->val);
+        mp[node] = copy;
+
+        for(Node* it: node->neighbors){
+            copy->neighbors.push_back(dfs(mp, it));
         }
-        Node* copyNode = new Node(node->val);
-        mp[node] = copyNode;
-
-        for(Node* n: node->neighbors){
-            copyNode->neighbors.push_back(dfs(n,mp));
-        }
-
-        return copyNode;
+        return copy;
     }
 
     Node* cloneGraph(Node* node) {
         map<Node*, Node*> mp;
-        return dfs(node, mp);
+        return dfs(mp, node);
     }
 };
