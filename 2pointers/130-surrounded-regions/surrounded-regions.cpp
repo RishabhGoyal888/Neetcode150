b4cbe63b0e0b33bdc35f10da74bc97bce44ad185
RishabhGoyal888 <159071663+RishabhGoyal888@users.noreply.github.com>
2025-01-18 16:36:49 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (57.12%) - LeetSync

--- 2pointers/130-surrounded-regions/surrounded-regions.cpp
+++ 2pointers/130-surrounded-regions/surrounded-regions.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
 
-    void dfs(int i, int j, vector<vector<char>>& board, vector<vector<int>>& vis, int n, int m, bool flag){
+    void dfs(int i, int j, vector<vector<char>>& board, vector<vector<int>>& vis, int n, int m){
         vis[i][j] = 1;
-        if(flag) board[i][j] = 'S';
-        else board[i][j] = 'X';
+        board[i][j] = 'S';
         int nrow[] = {-1,1,0,0};
         int ncol[] = {0,0,-1,1};
         for(int x=0;x<4;x++){
             int nr = i+nrow[x];
             int nc = j+ncol[x];
             if(nr >=0 && nr < n && nc >=0 && nc < m && vis[nr][nc]==0 && board[nr][nc]=='O'){
-                if(flag)
-                    dfs(nr,nc,board,vis,n,m,true);
-                else
-                    dfs(nr,nc,board,vis,n,m,false);
+                dfs(nr,nc,board,vis,n,m);
             }
         }
         
@@ -27,16 +23,16 @@ public:
         vector<vector<int>> vis(n, vector<int>(m,0));
         for(int i=0;i<n;i++){
             if(board[i][0] == 'O')
-                dfs(i,0,board, vis, n, m,true);
+                dfs(i,0,board, vis, n, m);
             if(board[i][m-1] == 'O')    
-                dfs(i,m-1,board, vis, n, m,true);
+                dfs(i,m-1,board, vis, n, m);
         }
 
         for(int j=0;j<m;j++){
             if(board[0][j] == 'O')
-                dfs(0,j,board, vis, n, m,true);
+                dfs(0,j,board, vis, n, m);
             if(board[n-1][j] == 'O')    
-                dfs(n-1,j,board, vis, n, m,true);
+                dfs(n-1,j,board, vis, n, m);
         }
 
         for(int i=0;i<n;i++){
