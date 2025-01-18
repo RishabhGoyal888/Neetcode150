966413e5850c8d8f36465447962ffa756a2ff917
RishabhGoyal888 <159071663+RishabhGoyal888@users.noreply.github.com>
2025-01-18 16:31:28 +0530

    Time: 85 ms (5.03%) | Memory: 19.3 MB (8.58%) - LeetSync

---
+++ 2pointers/130-surrounded-regions/surrounded-regions.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+
+    void dfs(int i, int j, vector<vector<char>>& board, vector<vector<int>>& vis, int n, int m, bool flag){
+        vis[i][j] = 1;
+        if(flag) board[i][j] = 'S';
+        else board[i][j] = 'X';
+        int nrow[] = {-1,1,0,0};
+        int ncol[] = {0,0,-1,1};
+        for(int x=0;x<4;x++){
+            int nr = i+nrow[x];
+            int nc = j+ncol[x];
+            if(nr >=0 && nr < n && nc >=0 && nc < m && vis[nr][nc]==0 && board[nr][nc]=='O'){
+                if(flag)
+                    dfs(nr,nc,board,vis,n,m,true);
+                else
+                    dfs(nr,nc,board,vis,n,m,false);
+            }
+        }
+        
+    }
+
+    void solve(vector<vector<char>>& board) {
+        int n = board.size();
+        int m = board[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            if(board[i][0] == 'O')
+                dfs(i,0,board, vis, n, m,true);
+            if(board[i][m-1] == 'O')    
+                dfs(i,m-1,board, vis, n, m,true);
+        }
+
+        for(int j=0;j<m;j++){
+            if(board[0][j] == 'O')
+                dfs(0,j,board, vis, n, m,true);
+            if(board[n-1][j] == 'O')    
+                dfs(n-1,j,board, vis, n, m,true);
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='O' && vis[i][j] == 0){
+                    cout<<i<<" "<<j;
+                    dfs(i,j,board, vis, n, m,false);
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='S'){
+                    board[i][j] = 'O';
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                cout<<board[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+};
